dc8b927a5804f2c02c4e11ebedf1340f00b8b353
sunying12 <sunyings@alumni.stanford.edu>
2026-06-07 19:12:46 -0700

    Add initial files
    Add initial files

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('zotnet-v1').then(cache =>
+      cache.addAll(['/', '/zotnet.html', '/manifest.json'])
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
